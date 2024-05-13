1b2d752d12ada94aaad4413748695a952a4ddd9b
Estrella <165815792+Estre05@users.noreply.github.com>
2024-05-13 17:12:13 -0500

    .

---
+++ index.js
@@ -0,0 +1,21 @@
+"Use strict";
+
+//Iniciar contador
+let claveCorrecta=3214;
+let num;
+let cont = 0;
+let maxInt=5;
+do{
+    cont++
+    num = Number(prompt("Ingrese la clave"));
+    if(cont >= maxInt){
+        alert("Bloqueo de tarjeta");
+        break;
+    }
+}while(num != claveCorrecta);
+if(num === claveCorrecta){
+    alert("Operación Exitosa ");
+}else{
+    alert("Clave incorrecta");
+}
+
